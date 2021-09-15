2bc2a79fd72765445c2d7837a8698f884757b529
SrixxCodes <designsbysrixx@gmail.com>
2021-09-15 21:37:27 +0100

    Player Movement and Animation
    - Finished Part 1 and 2 of tutorial
    - Incorporates Player Walking Animations
    - Incorporates Rigidbody movement in 4 directions
    - Uses blend trees for animations

---
+++ 2D Top-Down Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody2d;
+    private Vector3 change;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationMove();
+    }
+
+    void UpdateAnimationMove() 
+    { 
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            anim.SetFloat("moveX", change.x);
+            anim.SetFloat("moveY", change.y);
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        rigidbody2d.MovePosition(transform.position + change.normalized * speed * Time.fixedDeltaTime);
+    }
+
+}
