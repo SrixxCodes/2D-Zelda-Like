99b806d9bd3a8015485cf6936d6da3d653d22d6a
SrixxCodes <designsbysrixx@gmail.com>
2021-09-16 17:24:04 +0100

    Camera Bounds
    - Added bounds on X and Y for the camera

--- 2D Top-Down Project/Assets/Scripts/CameraMovement.cs
+++ 2D Top-Down Project/Assets/Scripts/CameraMovement.cs
@@ -6,13 +6,21 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
+    public Vector2 maxPos;
+    public Vector2 minPos;
 
     void FixedUpdate()
     {
         if (transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
+
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+
         }
     }
 }
