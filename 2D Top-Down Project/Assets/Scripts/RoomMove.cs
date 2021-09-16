19b122f6f1f58b570b25714288abd4ab20305cf6
SrixxCodes <designsbysrixx@gmail.com>
2021-09-16 21:58:17 +0100

    Place Texts!
    Added a new font
    Title Cards that can be edited

---
+++ 2D Top-Down Project/Assets/Scripts/RoomMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraMinChange;
+    public Vector2 cameraMaxChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
+
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam.minPos += cameraMinChange;
+            cam.maxPos += cameraMaxChange;
+            collision.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
+
+        }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+
+}
