38d33221450ab2ffb02cc9c58ec638cb05f4e9da
SrixxCodes <designsbysrixx@gmail.com>
2021-09-17 21:44:49 +0100

    PopUp and New File Made

---
+++ 2D Top-Down Project/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 yOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + yOffset;
+
+    }
+}
---
+++ 2D Top-Down Project/Assets/Scripts/Sign.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public GameObject question;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+                question.SetActive(true);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+                question.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+            if (question.activeInHierarchy)
+            {
+                question.SetActive(false);
+            }
+            else
+            {
+                question.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+            question.SetActive(false);
+        }
+    }
+
+}
