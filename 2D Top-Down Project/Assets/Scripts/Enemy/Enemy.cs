cf9c70588e6b671c290f42fcf152b61f4e06dc54
SrixxCodes <designsbysrixx@gmail.com>
2021-09-18 10:28:14 +0100

    Enemy AI
    - Walking
    - Inheritance of a class with some default attributes
    - Remember to add more things to this later

---
+++ 2D Top-Down Project/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+}
---
+++ 2D Top-Down Project/Assets/Scripts/Enemy/Log/Log.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform spawnPosition;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        CheckDistance();    
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+}
