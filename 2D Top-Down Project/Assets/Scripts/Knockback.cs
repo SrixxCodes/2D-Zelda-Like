d6dc2317dd242a49033d32fc33dbdf7e5ab53f9c
SrixxCodes <designsbysrixx@gmail.com>
2021-09-18 10:47:25 +0100

    Knockback

---
+++ 2D Top-Down Project/Assets/Scripts/Knockback.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public float thrust;
+    public float knockTime;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = collision.GetComponent<Rigidbody2D>();
+            if(enemy != null)
+            {
+                StartCoroutine(KnockCoroutine(enemy));
+            }
+        }
+    }
+
+    private IEnumerator KnockCoroutine(Rigidbody2D enemy)
+    {
+        Vector2 forceDirection = enemy.transform.position - transform.position;
+        Vector2 force = forceDirection.normalized * thrust;
+
+        enemy.velocity = force;
+        yield return new WaitForSeconds(knockTime);
+
+        enemy.velocity = Vector2.zero;
+    }
+
+
+}
