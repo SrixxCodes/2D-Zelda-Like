1331381b764f1872bf566bf22f14ed4d3b265117
SrixxCodes <designsbysrixx@gmail.com>
2021-09-18 08:52:41 +0100

    Pots!
    - Breakable Pots!

---
+++ 2D Top-Down Project/Assets/Scripts/Pot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Smash()
+    {
+        anim.SetBool("smash", true);
+        StartCoroutine(BreakCo());
+    }
+
+    IEnumerator BreakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+
+}
