1fbf014b2b72c2781d7ec5d368e6793b234ca630
SrixxCodes <designsbysrixx@gmail.com>
2021-09-24 20:04:40 +0100

    Signals System using Scriptable Objects
    - Using Signals, you can call and run functions all through the code
    - Also polished some UI bugs

---
+++ 2D Top-Down Project/Assets/Scripts/SignalsListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalsListener : MonoBehaviour
+{
+    public Signals signals;
+    public UnityEvent signalsEvent;
+
+    public void OnSignalsRaised()
+    {
+        signalsEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        signals.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        signals.DeRegisterListener(this);
+    }
+
+}
