634f3fa82c5f4da382cb1c7311882341696bcc56
SrixxCodes <designsbysrixx@gmail.com>
2021-09-27 19:34:17 +0100

    Fade In and Out
    - Can add different fades for different environments. Very cool feature.

--- 2D Top-Down Project/Assets/Scripts/SceneTransition.cs
+++ 2D Top-Down Project/Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,42 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject inPanel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity);
+            Destroy(inPanel, 1);
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") && !collision.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            // SceneManager.LoadScene(sceneToLoad);
         }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!asyncOperation.isDone) 
+        {
+            yield return null;
+        }
+
     }
 
 }
